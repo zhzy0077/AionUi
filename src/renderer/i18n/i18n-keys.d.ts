df687167992e02f2405b57c023e09a48690bcfd0
zhzy0077 <zhzy0077@users.noreply.github.com>
2026-03-16 12:33:11 +0800

    revert: remove unnecessary descriptionWebuiContext translation

--- src/renderer/i18n/i18n-keys.d.ts
+++ src/renderer/i18n/i18n-keys.d.ts
@@ -1131,7 +1131,6 @@ export type I18nKey =
   | 'settings.webui.currentPasswordPlaceholder'
   | 'settings.webui.currentPasswordRequired'
   | 'settings.webui.description'
-  | 'settings.webui.descriptionWebuiContext'
   | 'settings.webui.enable'
   | 'settings.webui.enableDesc'
   | 'settings.webui.featureChannelsDesc'
